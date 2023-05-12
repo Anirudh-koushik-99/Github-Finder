c8434c84a33ed8afedefa936743b7cee5911bf4a
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-05-13 00:08:16 +0530

    check commit

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -3,25 +3,25 @@ import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const fetchUsers = async () => {
-    const resposne = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await resposne.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      fetchUsers();
+    }, []);
+  
+    const fetchUsers = async () => {
+      const resposne = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      });
+  
+      const data = await resposne.json();
+  
+      setUsers(data);
+      setLoading(false);
+    };
 
   if (!loading) {
     return (
