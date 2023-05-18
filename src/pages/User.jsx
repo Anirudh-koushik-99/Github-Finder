d26dfc670c89e2ed71e8a8a1a1f4a83ab8b6dcbb
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-05-18 22:54:55 +0530

    Using Axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,7 +2,7 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { getUser, getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 import Spinner from "../components/layout/Spinner";
 import GithubContext from "../context/github/GithubContext";
 import RepoList from "../components/repos/RepoList";
@@ -16,11 +16,9 @@ function User() {
     dispatch({type: 'SET_LOADING'})
     const getUserData = async () => {
 
-      const userData  = await getUser(params.login)
-      dispatch({ type: 'GET_USER', payload: userData })
+      const userData  = await getUserAndRepos(params.login)
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData })
 
-      const userRepoData  = await getUserRepos(params.login)
-      dispatch({ type: 'GET_REPOS', payload: userRepoData })
     } 
 
     getUserData()
